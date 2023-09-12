7e699db882dc7e4abb24c2dc09a532defee860ea
AntonyBil <beletskiy.ant@gmail.com>
2023-09-12 21:24:08 +0300

    Initial commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by apple on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
